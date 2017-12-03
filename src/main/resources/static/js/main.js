d7a333953ed27ef55d1e53b83d1d2bc05c1b9c6e
Jeff Chien <jeff85@gmail.com>
2017-12-03 13:41:20 -0600

    make v-card the top-level flexbox item so that all cards are the same height (based on max stream description length)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,17 @@
 var streamComponent = {
     props: ['stream', 'gamePropMap'],
     template: `
-        <div class="stream_item" v-show="gamePropMap.hasOwnProperty(stream.gameName) && gamePropMap[stream.gameName].display" v-bind:id="'stream-' + stream.channelId">
-            <v-card>
-                <a v-bind:href="stream.channelUrl"><v-card-media v-bind:src="stream.previewUrl" height="180px" contain></v-card-media></a>
-                <v-card-text class="stream_card_text">
-                    <div class="stream_text">
-                        <div><strong>{{stream.displayName}}</strong></div>
-                        <div>{{stream.status}}</div>
-                        <div><strong>{{stream.gameName}}</strong></div>
-                        <div>{{stream.numViewers}} viewers</div>
-                    </div>
-                </v-card-text>
-            </v-card>
-        </div>
+        <v-card class="stream_item" v-show="gamePropMap.hasOwnProperty(stream.gameName) && gamePropMap[stream.gameName].display" v-bind:id="'stream-' + stream.channelId">
+            <a v-bind:href="stream.channelUrl"><v-card-media v-bind:src="stream.previewUrl" height="180px" contain></v-card-media></a>
+            <v-card-text class="stream_card_text">
+                <div class="stream_text">
+                    <div><strong>{{stream.displayName}}</strong></div>
+                    <div>{{stream.status}}</div>
+                    <div><strong>{{stream.gameName}}</strong></div>
+                    <div>{{stream.numViewers}} viewers</div>
+                </div>
+            </v-card-text>
+        </v-card>
     `
 };
 
